b86960f4fa6ce1ab40c8558d11e2ec817a6377a2
kevinlgreene94 <73610121+kevinlgreene94@users.noreply.github.com>
2020-11-30 13:24:54 -0700

    Add files via upload

---
+++ gallary/Fitness-BootstrapStudent/scripts/scripts.js
@@ -0,0 +1,10 @@
+/*
+Student Name: Kevin Greene
+File Name: script.js
+Date: 11/30/2020
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero') .height($(window).height());
+});
