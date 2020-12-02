158b0255342cb42f3e870c1a59412a95daeda864
kevinlgreene94 <73610121+kevinlgreene94@users.noreply.github.com>
2020-12-02 09:49:05 -0700

    Add files via upload

---
+++ gallary/WildRescue/scripts/script.js
@@ -0,0 +1,11 @@
+/*
+    Student Name:Kevin Greene
+    File Name: script.js
+    Date: 12/2/2020
+*/
+
+//jQuery for hero image to consume the header window space
+
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
