cb9ccc712e3832cf22016fa0d23a5f8c17f5f63e
kevinlgreene94 <73610121+kevinlgreene94@users.noreply.github.com>
2021-03-11 15:48:47 -0700

    Add files via upload

---
+++ gallary/VueLab/src/app/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <CalendarWeek/>
+    <CalendarEntry/>
+  </div>
+</template>
+
+<script>
+import CalendarWeek from './components/CalenderWeek.vue';
+import CalendarEntry from "./components/CalanderEntry.vue";
+export default {
+  name: 'App',
+  components:{
+    CalendarWeek,
+    CalendarEntry
+  }
+}
+</script>
+
+<style lang="scss">
+html, body, #app {
+  height: 100%;
+}
+
+#app {
+  background: #6e6e6e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
+}
+
+</style>
